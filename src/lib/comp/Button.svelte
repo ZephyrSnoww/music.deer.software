2d7ca98b53b574e7c283dbf992dba9eb415dbdbe
Zephyr <zephyr.snow@pm.me>
2024-11-09 18:22:09 -0500

    Wrote basic button element

---
+++ src/lib/comp/Button.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  let {
+    children,
+    onclick,
+    icon = false
+  }: {
+    children: () => any;
+    onclick?: (e: Event) => any;
+    icon?: boolean;
+  } = $props();
+</script>
+
+<button {onclick} class:{icon}>{@render children()}</button>
+
+<style>
+  :root {
+    --padding: 0 1em;
+    --background: var(--lime);
+    --color: var(--black);
+  }
+
+  button {
+    font-size: unset;
+    font-family: unset;
+    color: unset;
+    background: unset;
+    border: unset;
+    border-radius: unset;
+    margin: unset;
+    padding: unset;
+
+    background: var(--background);
+    color: var(--color);
+    padding: var(--padding);
+    border-radius: 3px;
+    cursor: pointer;
+    filter: brightness(0.7);
+
+    transition: var(--default-transition) filter;
+  }
+
+  button:hover {
+    filter: brightness(1);
+  }
+</style>
