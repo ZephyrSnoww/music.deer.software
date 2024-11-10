ba4d082fa588c7acb695bce66c5c5a18af1e647d
Zephyr <zephyr.snow@pm.me>
2024-11-10 13:58:14 -0500

    Wrote basic navbar

---
+++ src/routes/Nav.svelte
@@ -0,0 +1,71 @@
+<script>
+  import { state } from "$lib/state.svelte";
+  import NavButton from "./NavButton.svelte";
+</script>
+
+<!-- TODO Add subnav toggleability (probably using stores?) -->
+
+<div id="nav">
+
+  <a id="logo" href="/">
+    <img src="/favicon-outlined.png" alt="" />
+  </a>
+
+  <NavButton
+    label="Playlists"
+    onclick={() => {
+      state.subnav = "playlists";
+    }}>list</NavButton
+  >
+
+  <NavButton
+    label="Tags"
+    onclick={() => {
+      state.subnav = "tags";
+    }}>tag</NavButton
+  >
+
+  <NavButton href="/songs" label="Songs">music_note</NavButton>
+  <NavButton href="/artists" label="Artists">person</NavButton>
+</div>
+
+<style>
+  #nav {
+    background: black;
+    grid-area: nav;
+
+    display: flex;
+    flex-direction: column;
+    overflow: visible;
+  }
+
+  #logo {
+    width: 3em !important;
+    border-bottom-right-radius: 0.25em;
+  }
+
+  #logo img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+
+  #nav > * {
+    width: 100%;
+    aspect-ratio: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 2em;
+    text-decoration: none;
+    color: unset;
+    background: black;
+    filter: brightness(0.7);
+    transition-property: filter, background;
+  }
+
+  #nav > *:hover {
+    background: #0a0a0a;
+    filter: brightness(1);
+  }
+</style>
