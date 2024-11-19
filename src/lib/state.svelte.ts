5d4bf967e9dee0aa40381886deb0322e58fc78da
Zephyr <zephyr.snow@pm.me>
2024-11-19 14:56:57 -0500

    Added nowPlaying to app state

--- src/lib/state.svelte.ts
+++ src/lib/state.svelte.ts
@@ -1,3 +1,9 @@
-export const appState = $state({
-  subnav: ""
+import type { song } from "@prisma/client";
+
+export const appState: {
+  subnav: string;
+  nowPlaying?: song
+} = $state({
+  subnav: "",
+  nowPlaying: undefined
 });
