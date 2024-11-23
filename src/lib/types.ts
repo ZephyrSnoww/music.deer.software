9d40fda66b7bb285285da73d8034d130fbfa67ea
Zephyr <zephyr.snow@pm.me>
2024-11-23 18:43:19 -0500

    Wrote basic ClienData type

---
+++ src/lib/types.ts
@@ -0,0 +1,13 @@
+import type { Prisma } from "@prisma/client";
+
+export interface ClientData {
+  account?: Prisma.userGetPayload<{include: {
+    editablePlaylists: true,
+    favorites: true,
+    ownedPlaylists: true,
+    playData: true,
+    ratings: true,
+    tags: true,
+    uploadedSongs: true
+  }}>;
+}
