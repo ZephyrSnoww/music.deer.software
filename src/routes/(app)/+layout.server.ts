bb4c31e88dde4710e3aa72717efe257c3346b10e
Zephyr <zephyr.snow@pm.me>
2024-11-23 18:50:57 -0500

    Wrote basic layout data retreival

---
+++ src/routes/(app)/+layout.server.ts
@@ -0,0 +1,39 @@
+import { db } from '$lib/db.js';
+import type { ClientData } from '$lib/types.js';
+
+export async function load({ cookies, depends }) {
+  depends("data:account");
+
+  const usernameCookie = cookies.get("username");
+  const tokenCookie = cookies.get("token");
+
+  let data: ClientData = {};
+  let removeCookies = true;
+
+  if (usernameCookie && tokenCookie) {
+    let user = await db.user.findUnique({ where: { username: usernameCookie }, include: { credentials: true } });
+
+    if (user && user.credentials!.token == tokenCookie) {
+      removeCookies = false;
+      data.account = await db.user.findUnique({
+        where: { username: usernameCookie },
+        include: {
+          editablePlaylists: true,
+          favorites: true,
+          ownedPlaylists: true,
+          playData: true,
+          ratings: true,
+          tags: true,
+          uploadedSongs: true
+        }
+      });
+    }
+  }
+
+  if (removeCookies) {
+    cookies.delete("username", { path: "/" });
+    cookies.delete("token", { path: "/" });
+  }
+
+  return data;
+}
