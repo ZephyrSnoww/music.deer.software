4738b27e9c7da2d202d2a4b01b0705a1870ba487
Zephyr Snow <zephyr.snow@pm.me>
2024-11-24 12:07:56 -0700

    Removed broken import and re-implemented error catching

--- src/routes/(app)/sort-uploads/+page.server.ts
+++ src/routes/(app)/sort-uploads/+page.server.ts
@@ -1,11 +1,9 @@
 import { env } from "$env/dynamic/private";
 import { existsSync, mkdirSync, readdirSync, readFileSync } from "fs";
 import MP3Tag from "mp3tag.js";
-// @ts-expect-error Package has shit typing
-import { parseFile } from "music-metadata";
 
 export async function load() {
-  // try {
+  try {
     // CREATE FOLDERS IF THEY DONT EXIST
     if (!existsSync(env.LIBRARY_FOLDER)) {
       mkdirSync(env.LIBRARY_FOLDER);
@@ -30,7 +28,7 @@ export async function load() {
 
     console.log(fileData);
     return { unsortedFiles: fileData };
-  // } catch (error) {
-  //   return {}
-  // }
+  } catch (error) {
+    return
+  }
 }
