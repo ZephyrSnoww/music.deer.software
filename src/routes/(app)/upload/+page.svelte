587c49aa47b0ec705c34ba114952c78942b1b09e
Zephyr <zephyr.snow@pm.me>
2024-11-24 12:20:26 -0500

    Fixed upload page styles

--- src/routes/(app)/upload/+page.svelte
+++ src/routes/(app)/upload/+page.svelte
@@ -9,65 +9,75 @@
   let showWarning = $state(false);
 </script>
 
-{#if !loading}
-  <form
-    method="POST"
-    id="upload"
-    enctype="multipart/form-data"
-    use:enhance={() => {
-      loading = true;
-      let timeWarning = setTimeout(() => {
-        showWarning = true;
-      }, 5000);
-      return async ({ update, result }) => {
-        await update();
-        loading = false;
-        clearInterval(timeWarning);
-      };
-    }}
-  >
-    <div id="title">Upload or Enter URL</div>
-    <input type="file" accept="audio/*" name="file" />
-    <Input placeholder="Enter URL" name="url" --width="35em" />
-    <Button>Submit</Button>
-  </form>
+<div id="upload-page">
+  {#if !loading}
+    <form
+      method="POST"
+      id="upload"
+      enctype="multipart/form-data"
+      use:enhance={() => {
+        loading = true;
+        let timeWarning = setTimeout(() => {
+          showWarning = true;
+        }, 5000);
+        return async ({ update, result }) => {
+          await update();
+          loading = false;
+          clearInterval(timeWarning);
+        };
+      }}
+    >
+      <div id="title">Upload or Enter URL</div>
+      <input type="file" accept="audio/*" name="file" />
+      <Input placeholder="Enter URL" name="url" --width="35em" />
+      <Button>Submit</Button>
+    </form>
 
-  {#if form?.message}
-    {#if form.error}
-      <div id="error-box">
-        <div id="error-title">Error</div>
-        <div>{form.message}</div>
-      </div>
-    {:else}
-      <div id="message-box">
-        <div>{@html form.message}</div>
+    {#if form?.message}
+      {#if form.error}
+        <div id="error-box">
+          <div id="error-title">Error</div>
+          <div>{form.message}</div>
+        </div>
+      {:else}
+        <div id="message-box">
+          <div>{@html form.message}</div>
+        </div>
+      {/if}
+    {/if}
+  {:else}
+    <div class="box">
+      <div>Processing...</div>
+    </div>
+    {#if showWarning}
+      <div class="box">
+        <div>
+          The only reason this might take longer than a few seconds
+          <br />
+          is if you entered a playlist or album URL with lots of songs.
+          <br />
+          If that isn't the case and you're stuck waiting for a while,
+          <br />
+          you should probably reload the page and try again.
+          <br /><br />
+          If you <i>did</i> enter a playlist or album with lots of songs,
+          <br />
+          please be patient.
+        </div>
       </div>
     {/if}
   {/if}
-{:else}
-  <div class="box">
-    <div>Processing...</div>
-  </div>
-  {#if showWarning}
-    <div class="box">
-      <div>
-        The only reason this might take longer than a few seconds
-        <br />
-        is if you entered a playlist or album URL with lots of songs.
-        <br />
-        If that isn't the case and you're stuck waiting for a while,
-        <br />
-        you should probably reload the page and try again.
-        <br /><br />
-        If you <i>did</i> enter a playlist or album with lots of songs,
-        <br />
-        please be patient.
-      </div>
-    </div>
-  {/if}
-{/if}
+</div>
 
 <style>
+  #upload-page {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
   #upload {
     display: flex;
     flex-direction: column;
