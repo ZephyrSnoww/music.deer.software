728e75a16aa0551bd3fe8e8b278fc12068b5f0e0
Zephyr <zephyr.snow@pm.me>
2024-11-26 17:40:11 -0700

    Wrote basic framework for popups

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -2,19 +2,40 @@
   import type { ClientData } from "$lib/types";
   import { appState } from "$lib/state.svelte";
   import { goto } from "$app/navigation";
+  import { source } from "sveltekit-sse";
   import "../generic.css";
 
   import Nav from "$lib/comp/layout/Nav.svelte";
   import Playbar from "$lib/comp/layout/Playbar.svelte";
   import Search from "$lib/comp/layout/Search.svelte";
   import Subnav from "$lib/comp/layout/Subnav.svelte";
+  import { fade } from "svelte/transition";
 
   let { children, data }: { children: () => any; data: ClientData } = $props();
 
   if (!data.account) {
     goto("/login");
   }
+
+  let popups: { id: number; text: string }[] = $state([]);
+
+  const genericEvent = source("/events", {}).select("generic");
+  genericEvent.subscribe((text: string) => {
+    if (text) {
+      let id = Math.floor(Math.random() * 10000000000);
+      popups.push({ id, text: text });
+      setTimeout(() => {
+        popups = popups.filter((p) => p.id != id);
+      }, 5000);
+    }
+  });
 </script>
+
+<div id="popups-container">
+  {#each popups as popup}
+    <div class="popup" transition:fade>{popup.text}</div>
+  {/each}
+</div>
 
 <div id="app">
   <Nav />
@@ -35,6 +56,27 @@
 </div>
 
 <style>
+  #popups-container {
+    width: 100vw;
+    height: 100vh;
+    position: absolute;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    flex-direction: column;
+    padding-bottom: 6em;
+    gap: 1em;
+  }
+
+  .popup {
+    /* position: absolute; */
+    background: var(--black);
+    padding: 1em;
+    z-index: 100;
+    border: 1px solid var(--lime);
+    border-radius: 0.2em;
+  }
+
   #app {
     width: 100vw;
     height: 100vh;
