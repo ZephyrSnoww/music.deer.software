6e248a67ffa244de72d1cb7792379de014a844a3
Zephyr <zephyr.snow@pm.me>
2024-11-26 21:40:33 -0700

    Added unsorted file deletion

--- src/routes/(app)/sort-uploads/+page.server.ts
+++ src/routes/(app)/sort-uploads/+page.server.ts
@@ -1,8 +1,12 @@
 import { env } from "$env/dynamic/private";
-import { existsSync, mkdirSync, readdirSync, readFileSync } from "fs";
+import { clients } from "$lib/clients.js";
+import { fail, redirect } from "@sveltejs/kit";
+import { existsSync, mkdirSync, readdirSync, readFileSync, rmSync } from "fs";
 import MP3Tag from "mp3tag.js";
 
-export async function load() {
+export async function load({ depends }) {
+  depends("data:unsorted");
+
   try {
     // CREATE FOLDERS IF THEY DONT EXIST
     if (!existsSync(env.LIBRARY_FOLDER)) {
@@ -35,14 +39,46 @@ export async function load() {
 // ACTION HANDLING
 export const actions = {
   // FILE INFO SUBMISSION
-  submit: async ({ request }) => {
+  submit: async ({ request, cookies }) => {
+    const tokenCookie = cookies.get("token");
+    if (!tokenCookie) {
+      return redirect(300, "/login");
+    }
+    const emit = clients.get(tokenCookie);
+    if (!emit) {
+      return fail(500);
+    }
+
     const formData = await request.formData();
 
     let filename = formData.get("filename") as string | null;
+    let artists = formData.get("artists") as string | null;
+    let title = formData.get("title") as string | null;
+    let album = formData.get("album") as string | null;
+
+    console.log(`Submit ${filename}`);
+    console.log(`Artists: ${artists}`);
+    console.log(`Title: ${title}`);
+    console.log(`Album: ${album}`);
   },
 
   // FILE DELETION
-  delete: async ({ request }) => {
+  delete: async ({ request, cookies }) => {
+    const tokenCookie = cookies.get("token");
+    if (!tokenCookie) {
+      return redirect(300, "/login");
+    }
+    const emit = clients.get(tokenCookie);
+    if (!emit) {
+      return fail(500);
+    }
 
+    const formData = await request.formData();
+
+    let filename = formData.get("filename") as string | null;
+
+    rmSync(`${env.LIBRARY_FOLDER}/unsorted/${filename}`);
+    emit("generic", "");
+    emit("generic", `Deleted ${filename}`);
   }
 }
