0a9a811b2eabcc04e494275308e7027a11a461e4
Zephyr <zephyr.snow@pm.me>
2024-11-26 22:23:15 -0700

    Made very basic song listing page

---
+++ src/routes/(app)/songs/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import type { ClientData } from "$lib/types";
+
+  const { data }: { data: ClientData } = $props();
+</script>
+
+{#if data.songs}
+  {#each data.songs as song}
+    <div>{song.title}</div>
+  {/each}
+{/if}
