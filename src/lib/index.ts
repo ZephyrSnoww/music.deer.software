dffcac1213694cbe5f701a709fe51f7f1096e381
Zephyr <zephyr.snow@pm.me>
2024-11-29 16:17:16 -0700

    Added function to easily verify subsonic parameters

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,9 @@
 import type { MP3TagTagsV1Defined } from "mp3tag.js/types/id3v1/tags";
 import type { MP3TagTagsV2Defined } from "mp3tag.js/types/id3v2/tags";
+import { create } from "xmlbuilder2";
+import { db } from "./db";
+import { verify } from "argon2";
+import { createHash } from "crypto";
 
 export const availableTags: {
   name: string;
@@ -100,5 +104,94 @@ export function getSubsonicParams(url: URL): {
     version: params.get("v"),
     client: params.get("c"),
     format: params.get("f")
+  };
+}
+
+export async function verifySubsonicParams(url: URL): Promise<{
+  username: string | null;
+  password: string | null;
+  token: string | null;
+  salt: string | null;
+  version: string | null;
+  client: string | null;
+  format: string | null;
+  authenticated: boolean;
+  response?: string;
+}> {
+  // GET SUBSONIC PARAMETERS
+  const {
+    username,
+    password,
+    token,
+    salt,
+    version,
+    client,
+    format
+  } = getSubsonicParams(url);
+
+  let authenticated = false;
+  let response
+
+  // CHECK IF PARAMS WERE GIVEN
+  if (!(username && (password || (token && salt)) && version && client)) {
+    let missingParam = username ? (version ? (client ? false : "c") : "v") : "u";
+
+    response = create({
+      "subsonic-response": {
+        "@xmlns": "http://subsonic.org/restapi",
+        "@status": "failed",
+        "@version": "1.16.1",
+        "error": {
+          "@code": "10",
+          "@message": missingParam ? `missing parameter: '${missingParam}'` : "missing authentication"
+        }
+      }
+    }).end();
+  }
+  else {
+    // AUTHENTICATE USER
+    const user = await db.user.findUnique({ where: { username }, include: { credentials: true } });
+
+    // CHECK USER EXISTS
+    if (user) {
+      // IF PASSWORD GIVEN, CHECK IF CORRECT
+      if (password) {
+        if (await verify(user.credentials!.password, password)) {
+          authenticated = true;
+        }
+      }
+
+      // IF TOKEN GIVEN, CHECK IF CORRECT
+      let hashedPassword = createHash("md5").update(user.credentials!.password + salt).digest("hex");
+      if (hashedPassword == token) {
+        authenticated = true;
+      }
+    }
+
+    if (!authenticated) {
+      response = create({
+        "subsonic-response": {
+          "@xmlns": "http://subsonic.org/restapi",
+          "@status": "failed",
+          "@version": "1.16.1",
+          "error": {
+            "@code": "40",
+            "@message": "Wrong username or password"
+          }
+        }
+      }).end();
+    }
+  }
+
+  return {
+    username,
+    password,
+    token,
+    salt,
+    version,
+    client,
+    format,
+    authenticated,
+    response
   };
 }
