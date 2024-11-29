320d7daf9d1f6b9f5be74df03d660e7cfc5a5b6c
Zephyr <zephyr.snow@pm.me>
2024-11-28 18:30:36 -0700

    Added audio player to layout

--- src/lib/comp/layout/Playbar.svelte
+++ src/lib/comp/layout/Playbar.svelte
@@ -1,13 +1,30 @@
 <script lang="ts">
+  import { appState } from "$lib/state.svelte";
   import PlaybarActions from "./PlaybarActions.svelte";
   import PlaybarControls from "./PlaybarControls.svelte";
   import PlaybarSongInfo from "./PlaybarSongInfo.svelte";
+
+  let audioPlayer: HTMLAudioElement | undefined = $state();
+
+  $effect(() => {
+    if (appState.nowPlaying?.id && audioPlayer) {
+      audioPlayer.play();
+    }
+  });
 </script>
+
+{#if appState.nowPlaying}
+  <audio
+    bind:this={audioPlayer}
+    src={`/file/${appState.nowPlaying?.filename}`}
+    style:display="none"
+  ></audio>
+{/if}
 
 <div id="playbar">
   <PlaybarSongInfo />
 
-  <PlaybarControls />
+  <PlaybarControls {audioPlayer} />
 
   <PlaybarActions />
 </div>
