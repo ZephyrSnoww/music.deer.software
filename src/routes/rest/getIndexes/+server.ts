31e78d66e0a54237f97159c646f1df086ac88bdb
Zephyr <zephyr.snow@pm.me>
2024-11-29 16:50:51 -0700

    Wrote getIndexes subsonic endpoint

---
+++ src/routes/rest/getIndexes/+server.ts
@@ -0,0 +1,61 @@
+import { verifySubsonicParams } from "$lib";
+import { db } from "$lib/db.js";
+import { create } from "xmlbuilder2";
+
+export async function GET({ setHeaders, url }) {
+  const {
+    username,
+    password,
+    token,
+    salt,
+    version,
+    client,
+    format,
+    authenticated,
+    response
+  } = await verifySubsonicParams(url);
+
+  // SET HEADERS
+  setHeaders({
+    "Content-Type": "application/xml"
+  });
+
+  if (!authenticated) {
+    return new Response(response);
+  }
+
+  // ACTUAL RESPONSE HERE
+  // GET ARTISTS
+  const artists = await db.artist.findMany({ include: { _count: true } });
+  let artistData = [];
+
+  console.log(artists);
+
+  for (let artist of artists) {
+    artistData.push({
+      "@id": artist.name,
+      "@name": artist.name,
+      "@albumCount": artist._count.albums,
+      "@coverArt": "",
+      "@artistImageUrl": ""
+    });
+  }
+
+  // RETURN DATA
+  return new Response(create({
+    "subsonic-response": {
+      "@xmlns": "http://subsonic.org/restapi",
+      "@status": "ok",
+      "@version": "1.16.1",
+      "indexes": {
+        "@lastModified": new Date().valueOf().toString(),
+        "index": [
+          {
+            "@name": "#",
+            "artist": artistData
+          }
+        ]
+      }
+    }
+  }).end());
+}
