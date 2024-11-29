3998960c954a53a39088c2aaa71d64c69f24598e
Zephyr <zephyr.snow@pm.me>
2024-11-29 15:32:00 -0700

    Wrote basic song listing page

--- src/routes/(app)/songs/+page.svelte
+++ src/routes/(app)/songs/+page.svelte
@@ -1,11 +1,36 @@
 <script lang="ts">
+  import SongListing from "$lib/comp/SongListing.svelte";
+  import { appState } from "$lib/state.svelte";
   import type { ClientData } from "$lib/types";
 
   const { data }: { data: ClientData } = $props();
 </script>
 
+<div class="page-title">All Songs</div>
+
+<SongListing />
+
 {#if data.songs}
-  {#each data.songs as song}
-    <div>{song.title}</div>
-  {/each}
+  <div id="song-listing">
+    {#each data.songs as song}
+      <SongListing
+        {song}
+        currentlyPlaying={appState.nowPlaying?.id == song.id}
+      />
+    {/each}
+  </div>
 {/if}
+
+<style>
+  .page-title {
+    font-size: 2em;
+    font-weight: bold;
+    padding: 0.5em 0;
+  }
+
+  #song-listing {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+</style>
