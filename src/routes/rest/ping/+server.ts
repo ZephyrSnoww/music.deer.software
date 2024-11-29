f0d57421bde6c1c6e3223831ebfb7ebd2d80d515
Zephyr <zephyr.snow@pm.me>
2024-11-29 16:17:41 -0700

    Added ping subsonic endpoint

---
+++ src/routes/rest/ping/+server.ts
@@ -0,0 +1,34 @@
+import { verifySubsonicParams } from '$lib';
+import { create } from 'xmlbuilder2';
+
+export async function GET({ setHeaders, url }) {
+  const {
+    username,
+    password,
+    token,
+    salt,
+    version,
+    client,
+    format,
+    authenticated,
+    response
+  } = await verifySubsonicParams(url);
+
+  // SET HEADERS
+  setHeaders({
+    "Content-Type": "application/xml"
+  });
+
+  if (!authenticated) {
+    return new Response(response);
+  }
+
+  // ACTUAL RESPONSE HERE
+  return new Response(create({
+    "subsonic-response": {
+      "@xmlns": "http://subsonic.org/restapi",
+      "@status": "ok",
+      "@version": "1.16.1"
+    }
+  }).end());
+}
