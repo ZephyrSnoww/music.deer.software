c2e8a3be51c4dea45bbeb0b7c6aeae2ccfc8aeb0
Zephyr <zephyr.snow@pm.me>
2024-12-01 17:03:29 -0700

    Removed sorting page and made upload page automatically add songs to the server

--- src/routes/(app)/upload/+page.server.ts
+++ src/routes/(app)/upload/+page.server.ts
@@ -1,20 +1,28 @@
 import { env } from "$env/dynamic/private";
 import { clients } from "$lib/clients.js";
+import { db } from "$lib/db.js";
 import { fail, redirect } from "@sveltejs/kit";
 import { exec } from "child_process";
-import { existsSync, mkdirSync, readdirSync, writeFileSync } from "fs";
+import { existsSync, mkdirSync, readdirSync, readFileSync, renameSync, writeFileSync } from "fs";
+import MP3Tag from "mp3tag.js";
 
 export const actions = {
   default: async ({ cookies, request }) => {
     const formData = await request.formData();
 
+    const usernameCookie = cookies.get("username");
     const tokenCookie = cookies.get("token");
-    if (!tokenCookie) {
-      return redirect(300, "/login");
+    if (!tokenCookie || !usernameCookie) {
+      return redirect(302, "/login");
     }
     const emit = clients.get(tokenCookie);
     if (!emit) {
       return fail(500);
+    }
+
+    const user = await db.user.findUnique({ where: { username: usernameCookie } });
+    if (!user) {
+      return redirect(302, "/login");
     }
 
     let file = formData.get("file");
@@ -63,6 +71,7 @@ export const actions = {
       const ytdl = exec(`yt-dlp -o "${env.LIBRARY_FOLDER}/unsorted/%(creator,uploader,channel)s - %(title,fulltitle)s.%(ext)s" -x --audio-format mp3 --embed-metadata --embed-thumbnail ${url}`);
       let error = false;
       let filename = "";
+      let files: string[] = [];
 
       ytdl.once("spawn", (m: any) => {
         console.log("spawned");
@@ -84,7 +93,12 @@ export const actions = {
           z.match(/\[ExtractAudio\] Destination:/) ||
           z.match(/\[ExtractAudio\] Not converting audio/)
         ) {
+          filename = z.match(/\[ExtractAudio\] Destination: \.\/music\/unsorted\/(.+)/)?.[1] || "";
+          if (!filename) {
+            filename = z.match(/\[ExtractAudio\] Not converting audio \.\/music\/unsorted\/(.+?); file is already in target format/)?.[1] || "";
+          }
           emit("upload", `Saving ${filename ? filename : "file"}`);
+          files.push(filename);
         }
       });
 
@@ -103,14 +117,96 @@ export const actions = {
       await new Promise((resolve, reject) => {
         ytdl.once("close", (z: any) => {
           console.log("closed");
-          if (!error) {
-            emit("generic", "");
-            emit("generic", "Completed");
-          }
           emit("upload", "");
           resolve(z);
         });
       });
+
+      // CREATE FOLDERS IF THEY DONT EXIST
+      if (!existsSync(env.LIBRARY_FOLDER)) {
+        mkdirSync(env.LIBRARY_FOLDER);
+      }
+      if (!existsSync(`${env.LIBRARY_FOLDER}/library`)) {
+        mkdirSync(`${env.LIBRARY_FOLDER}/library`);
+      }
+
+      for (const file of files) {
+        emit("upload", `Reading tags from ${file}`);
+        let tags = new MP3Tag(readFileSync(`${env.LIBRARY_FOLDER}/unsorted/${file}`)).read();
+
+        let title = tags.v2?.TIT2;
+        let artist = tags.v2?.TPE1;
+        let album = tags.v2?.TALB || title;
+        let year = tags.v2?.TYER;
+        let genre = tags.v2?.TCON;
+        let trackNum = tags.v2?.TRCK;
+        let url = tags.v2?.TXXX?.find((t) => t.description == "purl")?.text;
+        // ALBUM ARTISTS
+        // DISC NUM
+        // COMMENTS?
+
+        // MAKE SURE FILENAME ISNT TAKEN
+        emit("upload", "Making sure filename isn't taken");
+        let existingFiles = readdirSync(`${env.LIBRARY_FOLDER}/library`);
+        let oldFilename = file;
+        let baseFilename = `${artist} - ${title}`;
+        let newFilename = baseFilename;
+
+        while (existingFiles.includes(`${newFilename}.mp3`)) {
+          newFilename = `${baseFilename}-${Math.floor(Math.random() * 10000)}`;
+        }
+
+        // MOVE FILE
+        emit("upload", `Renaming and moving ${oldFilename}`);
+        renameSync(`${env.LIBRARY_FOLDER}/unsorted/${oldFilename}`, `${env.LIBRARY_FOLDER}/library/${newFilename}.mp3`);
+
+        // ADD TO DATABASE
+        emit("upload", `Adding ${newFilename} to database`);
+        let song = await db.song.create({
+          data: {
+            filename: `${newFilename}.mp3`,
+            releaseDate: new Date(year || 0),
+            title: title || "Unknown Title",
+            uploader: { connect: { id: user.id } },
+            url: url,
+            trackNumber: Number(trackNum) || 1,
+            album: {
+              connectOrCreate: {
+                where: { name: album || "Unknown Album" },
+                create: {
+                  name: album || "Unknown Album",
+                  releaseDate: new Date(year || 0)
+                }
+              }
+            },
+            artists: {
+              connectOrCreate: {
+                where: { name: artist || "Unknown Artist" },
+                create: {
+                  name: artist || "Unknown Artist",
+                  albums: {
+                    connectOrCreate: {
+                      where: { name: album || "Unknown Album" },
+                      create: {
+                        name: album || "Unknown Album",
+                        releaseDate: new Date(year || 0)
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        });
+      }
+
+      emit("generic", "");
+      if (files.length == 1) {
+        emit("generic", `Added "${files[0]}" to the server!`);
+      }
+      else {
+        emit("generic", `Added ${files.length} files to the server!`);
+      }
 
       return;
     }
