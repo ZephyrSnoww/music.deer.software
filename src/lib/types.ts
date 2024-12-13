0059d96a9170381888f93069b317a5bf807fbf18
Zephyr <zephyr.snow@pm.me>
2024-12-13 14:04:45 -0500

    Fixed formatting

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -8,39 +8,39 @@ export interface CustomFormData {
 export interface ClientData {
   account?: Prisma.userGetPayload<{
     include: {
-      editablePlaylists: true,
-      favorites: true,
-      ownedPlaylists: true,
-      playData: true,
-      ratings: true,
-      tags: true,
-      uploadedSongs: true
+      editablePlaylists: true;
+      favorites: true;
+      ownedPlaylists: true;
+      playData: true;
+      ratings: true;
+      tags: true;
+      uploadedSongs: true;
     }
   }> | null;
   songs?: Prisma.songGetPayload<{
     include: {
-      album: true,
-      artists: true,
-      favoritedBy: true,
-      playData: true,
-      playlists: true,
-      ratings: true,
-      tags: true,
-      uploader: true
+      album: true;
+      artists: true;
+      favoritedBy: true;
+      playData: true;
+      playlists: true;
+      ratings: true;
+      tags: true;
+      uploader: true;
     }
   }>[];
   albums?: Prisma.albumGetPayload<{
     include: {
-      _count: { select: { tracks: true } },
-      artists: true
+      _count: { select: { tracks: true } };
+      artists: true;
     }
   }>[];
   playlists?: Prisma.playlistGetPayload<{
     include: {
-      _count: { select: { songs: true } },
-      owner: true,
-      editors: true,
-      songs: true
+      _count: { select: { songs: true } };
+      owner: true;
+      editors: true;
+      songs: true;
     }
   }>[];
 }
